456e68a193540ad46a744e66f7c1d1f99b77db5a
rcy <21010078@mail.ecust.edu.cn>
2024-04-11 10:12:44 +0800

    缓存

--- server/user/user_dao/get.go
+++ server/user/user_dao/get.go
@@ -4,19 +4,10 @@ import (
 	"go_crud/server/user/user_dao/service"
 	"go_crud/utils/mysql_db"
 	"gorm.io/gorm"
+	"log"
 )
 
 func GetUserByName(name string) []mysql_db.UserList {
-
-	//lock := service.GetRedLock(name)
-	//if err := lock.Lock(); err != nil {
-	//	log.Println("获取锁失败:", err)
-	//	return nil
-	//}
-	//defer lock.Unlock()
-	//go service.ContinueLock(lock)
-
-	db := service.DataBase.Session(&gorm.Session{NewDB: true})
 	var adminDataList []mysql_db.UserList
 
 	adminDataList = service.GetFromRedisByName(name)
@@ -25,6 +16,16 @@ func GetUserByName(name string) []mysql_db.UserList {
 		return adminDataList
 	} else {
 		//fmt.Println("从MySQL数据库中获取数据")
+		lock := service.GetRedLock(name)
+		if err := lock.Lock(); err != nil {
+			log.Println("获取锁失败:", err)
+			return nil
+		}
+		defer lock.Unlock()
+		go service.ContinueLock(lock)
+
+		db := service.DataBase.Session(&gorm.Session{NewDB: true})
+
 		err := db.Where("name = ?", name).Find(&adminDataList).Error
 		if err != nil {
 			return nil
--- server/user/user_dao/post.go
+++ server/user/user_dao/post.go
@@ -4,19 +4,20 @@ import (
 	"go_crud/server/user/user_dao/service"
 	"go_crud/utils/mysql_db"
 	"gorm.io/gorm"
+	"log"
 	"time"
 )
 
 func RecordPasswordWrong(userData mysql_db.UserList, tries uint) bool {
-	//// 方式一 分布式锁 强一致性
-	//lock := service.GetRedLock(userData.Name)
-	//// 尝试获取锁
-	//if err := lock.Lock(); err != nil {
-	//	log.Println("获取锁失败:", err)
-	//	return false
-	//}
-	//defer lock.Unlock()
-	//go service.ContinueLock(lock)
+	// 方式一 分布式锁 强一致性
+	lock := service.GetRedLock(userData.Name)
+	// 尝试获取锁
+	if err := lock.Lock(); err != nil {
+		log.Println("获取锁失败:", err)
+		return false
+	}
+	defer lock.Unlock()
+	go service.ContinueLock(lock)
 
 	db := service.DataBase.Session(&gorm.Session{NewDB: true})
 	userData.PasswordTry = tries
@@ -24,27 +25,26 @@ func RecordPasswordWrong(userData mysql_db.UserList, tries uint) bool {
 		userData.LockedUntil = time.Now().Add(time.Hour)
 		userData.PasswordTry = 0
 	}
-	db.Save(&userData)
 	service.ClearNameRedisCache(userData.Name)
+	db.Save(&userData)
 	return true
 }
 
 func SetUserStatus(userData mysql_db.UserList, status string) bool {
-	//// 方式一 分布式锁 强一致性
-	//lock := service.GetRedLock(userData.Name)
-	//// 尝试获取锁
-	//if err := lock.Lock(); err != nil {
-	//	log.Println("获取锁失败:", err)
-	//	return false
-	//}
-	//defer lock.Unlock()           // 函数结束时释放锁
-	//go service.ContinueLock(lock) // 启动一个协程来定期检查并延时锁
+	// 方式一 分布式锁 强一致性
+	lock := service.GetRedLock(userData.Name)
+	// 尝试获取锁
+	if err := lock.Lock(); err != nil {
+		log.Println("获取锁失败:", err)
+		return false
+	}
+	defer lock.Unlock()           // 函数结束时释放锁
+	go service.ContinueLock(lock) // 启动一个协程来定期检查并延时锁
 
 	db := service.DataBase.Session(&gorm.Session{NewDB: true})
 	userData.Status = status
-	//service.ClearNameRedisCache(userData.Name)
+	service.ClearNameRedisCache(userData.Name)
 	db.Save(&userData)
-	go service.SendMsgToMq(userData.Name, time.Now().Add(1000*time.Millisecond))
 	return true
 }
 
